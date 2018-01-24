0e4c47bb0aafae15843144b128038ea324c7244c
Musab <b_musab@hotmail.com>
2018-01-24 14:24:12 +0200

    Initial commit

---
+++ app/src/main/java/com/cardgame/musabbozkurt/Card.java
@@ -0,0 +1,19 @@
+package com.cardgame.musabbozkurt;
+
+import android.widget.Button;
+
+
+public class Card{
+
+	public int x;
+	public int y;
+	public Button button;
+	
+	public Card(Button button, int x,int y) {
+		this.x = x;
+		this.y=y;
+		this.button=button;
+	}
+	
+
+}
